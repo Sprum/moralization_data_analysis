0ac8744e5782944918c542f9f90be4d7b39ccf59
Sprum <108943595+Sprum@users.noreply.github.com>
2024-03-18 21:07:46 +0100

    added a few util filters for data handling

--- data_analysis/filter_sequence.py
+++ data_analysis/filter_sequence.py
@@ -1,9 +1,10 @@
-from typing import Type
+import re
+from typing import Type, Union, List
 
 import pandas as pd
-from pandas import DataFrame
+from pandas import DataFrame, Series
 
-from data_analysis.data_filter import DataFilter
+from data_analysis.data_filter import DataFilter, RegExFilter, DataFrameToSeriesList, ConcatMultipleDataFrames
 
 
 class FilterSequence:
@@ -30,20 +31,18 @@ class FilterSequence:
     ```
     """
 
-    def __init__(self, data: DataFrame, filter_stack: list[Type[DataFilter]]):
+    def __init__(self, data: DataFrame | Series | List, filter_stack: List[Type[DataFilter]]):
         """
         Initializes a FilterSequence object.
 
         Parameters:
-        - data (DataFrame): The input DataFrame to be filtered.
+        - :data:  DataFrame Series or List of those: The input Data to be filtered.
         - filter_stack (list[Type[DataFilter]]): A list of DataFilter classes representing the sequence of filters to be applied.
         """
         self.data = data
         self.filter_stack = filter_stack
 
-
-    def filter(self, *args, **kwargs):
-
+    def filter(self, *args, **kwargs) -> Union[DataFrame, Series]:
         """
          Executes the filter sequence on the provided DataFrame
          Returns:
@@ -52,6 +51,21 @@ class FilterSequence:
         result = self.data
 
         for data_filter in self.filter_stack:
-            result = data_filter(result).filter()
+            result = data_filter(result).filter(*args, **kwargs)
 
         return result
+
+
+if __name__ == "__main__":
+    df = pd.read_csv("E:\Coding\moralization\data\output\DE-Gerichtsurteile-NEG_lemmatized.csv")
+    df2 = pd.read_csv("E:\Coding\moralization\data\output\DE-Kommentare-NEG_lemmatized.csv")
+    df3 = pd.read_csv("E:\Coding\moralization\data\output\DE-Leserbriefe-NEG_lemmatized.csv")
+    dfs = [df, df2, df3]
+    r_pat = r'freiheit'
+
+    for d in dfs:
+        print(len(d))
+    seq = FilterSequence(dfs, [ConcatMultipleDataFrames])
+
+    outp = seq.filter(r_pattern=r_pat)
+    print(outp)
